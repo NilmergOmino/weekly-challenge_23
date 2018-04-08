c1eb67ddf514a0cce0028ecc0b90ed926aea2e68
NilmergOmino <nilmerg.omino@gmail.com>
2018-04-08 12:59:35 +0200

    nav styles started

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -74,6 +74,36 @@ __webpack_require__(1);
 
 var $ = __webpack_require__(2);
 
+$(document).on('scroll', function () {
+    var scrollTop = $(document).scrollTop();
+    if (scrollTop > 5) {
+        $('#navbar').addClass('-sticky');
+    } else {
+        $('#navbar').removeClass('-sticky');
+    }
+});
+
+$('#trigger-nav').on('click', function () {
+    $('#navbar').toggleClass('-active');
+    $('#trigger-nav').toggleClass('-active');
+});
+
+// $(document).ready(function(){
+//     $('._js-anchor').on('click', function(event){
+//         $('#menu').removeClass('-toggle');
+//         $('#trigger-menu').removeClass('-active');
+//         let target = this.hash.slice(1),
+//             hash = "#",
+//             targetTop = 0;
+//         if(target.length){
+//             hash = this.hash;
+//             targetTop = $('#'+target).offset().top;
+//         }
+//         $('html, body').animate({ scrollTop: targetTop }, 600, history.pushState(null,null,hash));
+//         event.preventDefault();
+//     })
+// })
+
 /***/ }),
 /* 1 */
 /***/ (function(module, exports) {
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,34 @@
 import '../scss/style.scss';
 let $ = require('jquery');
+
+
+$(document).on('scroll', function(){
+    let scrollTop = $(document).scrollTop();
+    if (scrollTop > 5){
+        $('#navbar').addClass('-sticky');
+    }
+    else{
+        $('#navbar').removeClass('-sticky');
+    }
+})
+
+$('#trigger-nav').on('click', function(){
+    $('#navbar').toggleClass('-active');
+    $('#trigger-nav').toggleClass('-active');
+})
+
+// $(document).ready(function(){
+//     $('._js-anchor').on('click', function(event){
+//         $('#menu').removeClass('-toggle');
+//         $('#trigger-menu').removeClass('-active');
+//         let target = this.hash.slice(1),
+//             hash = "#",
+//             targetTop = 0;
+//         if(target.length){
+//             hash = this.hash;
+//             targetTop = $('#'+target).offset().top;
+//         }
+//         $('html, body').animate({ scrollTop: targetTop }, 600, history.pushState(null,null,hash));
+//         event.preventDefault();
+//     })
+// })
